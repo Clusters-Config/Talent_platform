94e8b090b736157e61b52dc3185ccf248d035218
Aditya-NV-06 <adityanv4@gmail.com>
2025-01-28 12:30:41 +0530

    feat: add validation for new talent entries using Talentlist type

--- backend/src/controllers/talent.control.js
+++ backend/src/controllers/talent.control.js
@@ -1,7 +1,9 @@
 import { Talent } from "../exports.js";
+import { Talentlist } from "../types.js";
 
 
 const postTalent = async (req,res) =>{
+    try { 
     const {name,label,role,level,avail,location} = req.body;
 
     const newTalent = new Talent({ 
@@ -13,7 +15,15 @@ const postTalent = async (req,res) =>{
         location
     })
 
-    try { 
+    const parsetalent = Talentlist.safeParse(newTalent);
+
+    if(!parsetalent.success){ 
+        res.status(400).json({ 
+            msg:parsetalent.error
+        })
+        return;
+    }
+   
         await newTalent.save();
         res.status(200).json({ 
             msg:'Talent posted in the db'
