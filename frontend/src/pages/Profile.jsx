2ef829b791b7225accda2fdcdb75bba2dcfcf3de
Aditya-NV-06 <adityanv4@gmail.com>
2025-02-21 09:36:40 +0530

    feat: remove unused useNavigate import in Profile component

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 async function fetchData(name) {
   try {
@@ -16,7 +16,7 @@ async function fetchData(name) {
 }
 
 const ProfilePage = () => {
-   const navigate = useNavigate();
+   
   const [data, setData] = useState([]);
   console.log(data)
   useEffect(()=>{ 
